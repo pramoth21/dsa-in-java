88c6568504972d343adf9ae1aba59a955ff6e8ad
Pramoth Piumal <pramothpiumal7@gmail.com>
2026-09-24 15:57:17 +0530

    DSA Stack

---
+++ src/stack/StackUsingArray.java
@@ -0,0 +1,25 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackUsingArray {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+
+        //System.out.println(stack.empty());
+
+        stack.push("Attack on Titan");
+        stack.push("Bleach");
+        stack.push("86");
+        stack.push("Black clover");
+
+        System.out.println(stack);
+        //System.out.println(stack.empty());
+
+        String myFavAnime = stack.pop();
+        System.out.println("Favourite anime: " + myFavAnime);
+
+        System.out.println("what now peek on stack: " + stack.peek());
+        System.out.println(stack);
+    }
+}
